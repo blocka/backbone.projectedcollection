27a09c46ca1aee7e5ccc546e8d04ce27ab760574
Avi Block <atblock@gmail.com>
2013-12-19 10:53:22 -0500

    Initial Commit

---
+++ ProjectedCollection.js
@@ -0,0 +1,34 @@
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(['backbone'], factory);
+	} else {
+		// Browser globals
+		root.amdWeb = factory(root.Backbone);
+	}
+}(this, function (Backbone) {
+	var ProjectedCollection = function(collection,options) {
+		var sourceCollection = collection;
+
+		var targetCollection = new (options.collection || Backbone.Collection.extend({
+			model: options.model || Backbone.Model
+		}))();
+
+		var _refresh = function() {
+			var result = options.projection.call(sourceCollection);
+			targetCollection.set(result);
+		};
+
+		_refresh();
+
+		sourceCollection.on('all',_refresh);
+
+		targetCollection.dispose = function() {
+			sourceCollection.off(null,_refresh);
+		};
+
+		return targetCollection;
+	};
+
+	return ProjectedCollection;
+}));
